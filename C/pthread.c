45ff3ea53c5578fc09936094a7868fb9d0f6f244
david <you@example.com>
2014-11-04 12:28:04 -0500

    more C threads

--- C/pthread.c
+++ C/pthread.c
@@ -1,6 +1,46 @@
 #include <pthread.h>
 #include <stdio.h>
 
+/*  Compile and link with -pthread 
+
+
+for example  "gcc -pthread -g -o thread pthread.c "
+
+*/
+
+
+
+/* This is our third thread function.  It is like main(), but for a thread */
+void *threadFunc(void *arg){
+	char *str;
+	int i = 0;
+
+	str=(char*)arg;
+
+	while(i < 10 )
+	{
+		usleep(1);
+		printf("threadFunc says: %s\n",str);
+		++i;
+	}
+
+	return NULL;
+}
+
+void printer(){
+
+int z ;
+printf("This function will increment an integer") ;
+
+while(++z < 10000000);
+
+printf("done, z equals %d", z );
+}
+
+
+
+
+
 /* this function is run by the second thread */
 void *inc_x(void *x_void_ptr){
 
@@ -28,11 +68,14 @@ int main(){
 	/* create a second thread which executes inc_x(&x) */
 	if(pthread_create(&inc_x_thread, NULL, inc_x, &x) != 0) {  /*passes the new tread the reference to the thread, 
 
-       int pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine) (void *), void *arg);
+       int pthread_create(   pthread_t *thread,   const pthread_attr_t *attr,     void *(*start_routine) (void *),     void *arg);
  */
 		fprintf(stderr, "Error creating thread \n");
 		return 1;
 	}
+	
+	printf("the second thread has incremented x"); 
+
 	/* increment y to 100 in the first thread */
 	while(++y < 100);
 	printf("y increment finished\n");
@@ -45,8 +88,61 @@ int main(){
 	/* show the results - x is now 100 thanks to the second thread */
 	printf("x: %d, y: %d\n", x, y);
 
+
+	/* this is our third thread */
+		
+	pthread_t pth;	// this is our thread identifier
+	int i = 0;  //integer
+	/* Create worker thread */
+	pthread_create(&pth,NULL,threadFunc,"processing...");  
+	/* wait for our thread to finish before continuing */
+	pthread_join(pth, NULL /* void ** return value could go here */);
+
+	while(i < 10 ){
+		usleep(1);
+		printf("third is running... step %d \n" , i);
+		++i;
+	}
+
+
+	pthread_t pto;
+	pthread_create(&pth,NULL,printer,"processing...");  
+
+
+
+
+
+
+
+
+
+
+
 	return 0;
 
+}
+
+
+/* another example    */
+
+
+
+int mainTwo(void){
+	pthread_t pth;	// this is our thread identifier
+	int i = 0;
+	/* Create worker thread */
+	pthread_create(&pth,NULL,threadFunc,"processing...");
+	/* wait for our thread to finish before continuing */
+	pthread_join(pth, NULL /* void ** return value could go here */);
+
+	while(i < 10 )
+	{
+		usleep(1);
+		printf("main() is running...\n");
+		++i;
+	}
+
+	return 0;
 }
 
 
