0fea07767cc68ed55b346dd311cce80c95ca7f5f
david <davidawad64@gmail.com>
2014-11-28 04:47:09 -0500

    vastly better ruby

--- ruby/prog.rb
+++ ruby/prog.rb
@@ -1,21 +1,10 @@
 puts "Enter the string you want to search "
 input = gets.chomp
 
-charHash = Hash.new
-
-input.split("").each do |i| 
-	if charHash.has_key?(i)
-		puts "incrementing"
-		charHash[i]+=1
-	else
-		puts"storing"
-		charHash.store(i, 1)
-	end
-end
+char_hash = input.each_char.with_object(Hash.new(0)) { |i, h| h[i] += 1 }
 
 goc = "" 
 max = 0 
-charHash.each { |key,value| goc = key  if value > max  }
-
+char_hash.each { |key,value| goc = key  if value > max  }
 
 puts "The character #{goc} occurs the most frequently"
