896e6928c425c95ccf20c6dbdd5f8ef775d45eed
david <davidawad64@gmail.com>
2014-11-28 04:56:09 -0500

    better ruby algorithm

--- ruby/prog.rb
+++ ruby/prog.rb
@@ -1,3 +1,4 @@
+#!/usr/bin/env ruby
 puts "Enter the string you want to search "
 input = gets.chomp
 
@@ -5,6 +6,13 @@ char_hash = input.each_char.with_object(Hash.new(0)) { |i, h| h[i] += 1 }
 
 goc = "" 
 max = 0 
-char_hash.each { |key,value| goc = key  if value > max  }
+
+char_hash.each do |key,value| 
+  if value > max then
+    max = value
+    goc = key
+  end
+end
+
 
 puts "The character #{goc} occurs the most frequently"
